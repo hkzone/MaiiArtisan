0d86fe0805c9d75f780c4496d58b7247ef0ddd02
Vitaly <vehicle.ua@gmail.com>
2021-10-25 10:54:53 +0800

    bug fix

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ process.on('unhandledRejection', (err) => {
   });
 });
 
-// process.on('SIGTERM', () => {
-//   console.log('SIGTERM RECEIVED. Shutting down gracefully');
-//   server.close(() => {
-//     console.log('💥 Process terminated!');
-//   });
-// });
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
