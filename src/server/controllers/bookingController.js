37de9355aa4857dcc3bb98b264461677dab86dd8
Vitaly <vehicle.ua@gmail.com>
2022-01-10 14:06:29 +0800

    heroku fix

--- src/server/controllers/bookingController.js
+++ src/server/controllers/bookingController.js
@@ -75,32 +75,27 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 //TODO: CHANGE TO THIS AFTER DEPLOYMENT
 const createBookingCheckout = async (session) => {
-  const lineItems1 = await stripe.checkout.sessions.retrieve(session.id, {
+  const { lineItems } = await stripe.checkout.sessions.retrieve(session.id, {
     expand: ['line_items.data.price.product'],
   });
 
-  console.log(lineItems1);
+  console.log(lineItems);
 
-  console.log(
-    '@@@@@',
-    await stripe.checkout.sessions.listLineItems(session.id)
-  );
-
-  const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
-  console.log('lineItems', lineItems);
+  // const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
+  // console.log('lineItems', lineItems);
 
   const user = (await User.findOne({ email: session.customer_email })).id;
   const totalAmount = session.amount_total / 100;
-  // const orderItems = lineItems.data.map((el) => {
-  //   console.log(el);
-  //   return {
-  //     product: el.price.product.metadata.id,
-  //     qty: el.quantity,
-  //     customColor: el.price.product.metadata.customColor,
-  //     customFlavor: el.price.product.metadata.customFlavor,
-  //     customMessage: el.price.product.metadata.customMessage,
-  //   };
-  // });
+  const orderItems = lineItems.data.map((el) => {
+    console.log('element', el);
+    return {
+      product: el.price.product.metadata.id,
+      qty: el.quantity,
+      customColor: el.price.product.metadata.customColor,
+      customFlavor: el.price.product.metadata.customFlavor,
+      customMessage: el.price.product.metadata.customMessage,
+    };
+  });
 
   // await Booking.create({ user, orderItems, totalAmount });
 };
